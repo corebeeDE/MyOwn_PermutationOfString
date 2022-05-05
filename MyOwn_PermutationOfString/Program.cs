ef400ab2955d598b0f0f5ae44b92502db2459c46
Korbinian Offensperger <korbinian_offensperger@yahoo.de>
2022-05-05 23:06:12 +0200

    ~v0.95b: refactored version.

--- MyOwn_PermutationOfString/Program.cs
+++ MyOwn_PermutationOfString/Program.cs
@@ -11,46 +11,36 @@ namespace MyOwn_PermutationOfString
         static void Main()
         {
             // Origin of this project is codewars: https://www.codewars.com/kata/5254ca2719453dcc0b00027d/
-
-            // First permutation:
-            string temp = "2017";
-            GetPermutation(temp.ToCharArray(), 0, temp.Length - 1);
-            foreach (var item in allPermutations)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("------------------------------");
-
-            // New permutations:
-            allPermutations.Clear();
-            temp = "513";
-            GetPermutation(temp.ToCharArray(), 0, temp.Length - 1);
-            foreach (var item in allPermutations)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("------------------------------");
+            GetPermutation("2017", 0);
+            allPermutations.ForEach(x => Console.WriteLine(x));
         }
 
         // Global helping variable
         public static List<string> allPermutations = new List<string>();
 
-        // Actual perm
-        private static void GetPermutation(char[] inputArray, int helper, int arrayLength)
+        /// <summary>
+        /// Takes given char array and permutates all options.
+        /// </summary>
+        /// <param name="inputString">String to be permutated.</param>
+        /// <param name="helper">Helping variable.</param>
+        private static void GetPermutation(string inputString, int helper)
         {
-            if (helper == arrayLength)
+            char[] inputArray = inputString.ToCharArray();
+
+            if (helper == inputString.Length)
             {
                 if(!allPermutations.Contains(new string(inputArray)))
                     allPermutations.Add(new string(inputArray));
             }
             else
             {
-                for (int ctr = helper; ctr <= arrayLength; ctr++)
+                for (int ctr = helper; ctr < inputString.Length; ctr++)
                 {
                     var temp = inputArray[helper];
                     inputArray[helper] = inputArray[ctr];
                     inputArray[ctr] = temp;
-                    GetPermutation(inputArray, helper + 1, arrayLength);
+                    inputString = new string(inputArray);
+                    GetPermutation(inputString, helper + 1);
                 }
             }
         }
